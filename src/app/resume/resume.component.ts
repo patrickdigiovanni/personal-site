37cabf08c9ab5eb587174a73d8da9eb38f906130
Patrick DiGiovanni <patrickdigiovanni77@gmail.com>
2018-04-06 02:37:03 -0400

    attempted production build

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -36,7 +36,7 @@ data = [{
       "company": "Deepfelt Visions",
       "position": "Graphic Artist / Photographer",
       "website": "https://www.facebook.com/deepfeltvisions/",
-      "startDate": "2010-01-01",
+      "startDate": "2010",
       "summary": "Designed flyers, logos, and business cards.\nUsed software and hardware tools to manipulate photos into abstract compositions.\nExperimented with 3D modeling and animation using both graphical and code-based interfaces.\nDeveloped Oscillation Painter, a web-based tool for creating unique abstract art and live animations\nCreated and maintained a Facebook page with 950+ organic followers.",
       "highlights": [],
       "endDate" : "Present"
@@ -45,56 +45,56 @@ data = [{
       "company": "Literacy Volunteers of Rochester, Inc.",
       "position": "Digital Literacy Navigator",
       "website": "https://literacyrochester.org/",
-      "startDate": "2016-03-01",
+      "startDate": "2016",
       "summary": "Assisted individuals in libraries and other community centers with computer-related tasks.\nCommon scenarios included: creating resumes; submitting online job applications; creating and using email and other online services; using digital media as a tool for research and education.\nWorked to bridge the \“digital divide\” by empowering customers in areas of concentrated poverty, fostering core digital literacy skills.",
       "highlights": [],
-      "endDate": "2017-12-01"
+      "endDate": "2017"
     },
     {
       "company": "FedEx Office",
       "position": "Production Coordinator",
       "website": "https://www/fedexoffice.com",
-      "startDate": "2006-11-01",
+      "startDate": "2006",
       "summary": "Interacted directly with customers to create, track, and tender orders through computerized POS system, and performed end-of-day closing procedures.\nPlanned and executed all production related tasks, including scanning, printing, folding, punching, collating, and stapling of documents.\nPrepared documents for printing in various formats.\nCalibrated and maintain machines for proper color and quality of output.\nPrinted and finished large-format signs, banners, etc.\nGained hundreds of hours of valuable training in hardware, software and customer service.\nHelped store earn a \"Purple Promise\" award for providing an outstanding customer experience by initiating and coordinating a major order.",
       "highlights": [],
-      "endDate": "2010-09-01"
+      "endDate": "2010"
     },
     {
       "company": "Amigos De Los Animales",
       "position": "Shelter Worker",
       "website": "https://www.facebook.com/adlapr/?rc=p",
-      "startDate": "2014-01-01",
+      "startDate": "2014",
       "summary": "Assisted in the maintenance and care of a shelter housing 65-100 rescued street dogs.",
       "highlights": [],
-      "endDate": "2014-12-01"
+      "endDate": "2014"
     },
     {
       "company": "Mystic Morning Farm",
       "position": "Farm Assistant",
       "website": "https://www.facebook.com/Mystic-Morning-Farm-214829198597652/",
-      "startDate": "2010-01-01",
+      "startDate": "2010",
       "summary": "Planted, harvested and watered vegetable crops, and prepared them for sale in local markets.\nStaffed proprietary farm stand and booths at various farmers’ markets.",
       "highlights": [],
-      "endDate": "2012-01-01"
+      "endDate": "2012"
     },
     
     {
       "company": "Ahli Baba's Kabob Shop",
       "position": "Grill Cook /  Cashier",
       "website": "https://www.yelp.com/biz/ahli-babas-kabob-shop-burlington",
-      "startDate": "2003-05-01",
+      "startDate": "2003",
       "summary": "Prepared and served short order Mediterranean cuisine.\nMaintained a clean kitchen and dining area.\nOperated cash register and performed end-of-day closing procedures.",
       "highlights": [],
-      "endDate": "2012-09-01"
+      "endDate": "2012"
     },
     {
       "company": "Greater Burlington YMCA",
       "position": "Older School-Age Coordinator",
       "website": "https://www.gbymca.org/",
-      "startDate": "1998-10-01",
+      "startDate": "1998",
       "summary": "Developed and facilitated after school curriculum for children grades 4-6.\nTransported children to/from YMCA\nSupervised Assistant Counselors\nParticipated in cross-site coordinator meetings\nCoordinated inter-mural activities with other YMCA programs\nWrote newsletter to parents/acted as liaison between parents and program director.\nPlanned field trips, games, art projects, and other diversions.\nDesigned and supervised community service projects and fundraising activities",
       "highlights": [],
-      "endDate": "2002-06-01"
+      "endDate": "2002"
     }
   ],
   "education": [
@@ -102,28 +102,28 @@ data = [{
       "institution": "State University of New York Empire State College",
       "area": "",
       "studyType": "Bachelor of Science - BS",
-      "startDate": "2014-12-31",
+      "startDate": "2014",
       "gpa": "",
       "courses": [],
-      "endDate": "2017-12-31"
+      "endDate": "2017"
     },
     {
       "institution": "Monroe Community College",
       "area": "",
       "studyType": "",
-      "startDate": "2004-12-31",
+      "startDate": "2004",
       "gpa": "",
       "courses": [],
-      "endDate": "2005-12-31"
+      "endDate": "2005"
     },
     {
       "institution": "Community College of Vermont",
       "area": "",
       "studyType": "",
-      "startDate": "1997-12-31",
+      "startDate": "1997",
       "gpa": "",
       "courses": [],
-      "endDate": "1999-12-31"
+      "endDate": "1999"
     }
   ],
   "skills": [
@@ -391,10 +391,10 @@ data = [{
   "projects": [
     {
       "name": "Oscillation Painter",
-      "startDate": "Invalid date",
+      "startDate": "",
       "summary": "Apr 2015",
       "url": "",
-      "endDate": "Invalid date"
+      "endDate": ""
     }
   ]
 }];
